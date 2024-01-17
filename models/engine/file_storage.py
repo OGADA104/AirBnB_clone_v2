e4e889a3fccda842567ebe5bd50069396f15bfc7
MICHAEL <michaelogada104@gmail.com>
2024-01-17 21:34:38 +0000

    task 7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from collections import OrderedDict
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
-    __objects = {}
+    __objects = OrderedDict()
 
     def delete(self, obj=None):
         """delete object"""
