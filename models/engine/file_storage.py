79a0ad9464565bbe30219533215b8b49bdb1c066
MICHAEL <michaelogada104@gmail.com>
2024-01-17 18:57:05 +0000

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,13 +8,28 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def delete(self, obj=None):
+        """delete object"""
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            del self.__objects[key]
+        else:
+            pass
+
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            key = "{}".format(cls.__name__)
+            return {obj_key: obj for obj_key,
+                    obj in self.__objects.items() if obj_key.startswith(key)}
+        else:
+            return list(self.__objects.values())
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+        return self.__objects
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -40,11 +55,17 @@ class FileStorage:
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
                   }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
+        if self.__file_path:
+            try:
+                with open(self.__file_path, 'r') as file:
+                    objects_dict = json.load(file)
+                    for key, obj_dict in objects_dict.items():
+                        class_name, obj_id = key.split('.')
+                        obj_class = classes.get(class_name)
+                        if obj_class:
+                            obj_instance = obj_class(**obj_dict)
+                            self.__objects[key] = obj_instance
+            except FileNotFoundError:
+                pass
+        else:
             pass
