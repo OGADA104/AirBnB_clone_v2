ddd0c59a858c3265e994b303445468149818269d
Sgechuki <sgechuki@outlook.com>
2024-01-17 22:36:30 -0800

    Revert "task 7"
    This reverts commit e4e889a3fccda842567ebe5bd50069396f15bfc7.
    
    Rollback to before task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from collections import OrderedDict
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
-    __objects = OrderedDict()
+    __objects = {}
 
     def delete(self, obj=None):
         """delete object"""
