e41820e8663cbd5ceae5bf13abc2afad860f3d28
Sgechuki <sgechuki@outlook.com>
2024-01-17 22:37:11 -0800

    Revert "task 5"
    This reverts commit 79a0ad9464565bbe30219533215b8b49bdb1c066.
    
    Rollback to before task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,28 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def delete(self, obj=None):
-        """delete object"""
-        if obj is not None:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            del self.__objects[key]
-        else:
-            pass
-
-    def all(self, cls=None):
+    def all(self):
         """Returns a dictionary of models currently in storage"""
-        if cls is not None:
-            key = "{}".format(cls.__name__)
-            return {obj_key: obj for obj_key,
-                    obj in self.__objects.items() if obj_key.startswith(key)}
-        else:
-            return list(self.__objects.values())
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
-        return self.__objects
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -55,17 +40,11 @@ class FileStorage:
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
                   }
-        if self.__file_path:
-            try:
-                with open(self.__file_path, 'r') as file:
-                    objects_dict = json.load(file)
-                    for key, obj_dict in objects_dict.items():
-                        class_name, obj_id = key.split('.')
-                        obj_class = classes.get(class_name)
-                        if obj_class:
-                            obj_instance = obj_class(**obj_dict)
-                            self.__objects[key] = obj_instance
-            except FileNotFoundError:
-                pass
-        else:
+        try:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                        self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
             pass
