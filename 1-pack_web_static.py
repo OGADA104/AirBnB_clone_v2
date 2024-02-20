5e4579518c079c639da666af4638ed8320db9a65
MICHAEL <michaelogada104@gmail.com>
2024-02-20 07:34:28 +0300

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
-"""Generates a .tgz archive from the contents of the web_static folder."""
+"""
+Fabric script that generates a .tgz archive from the contents of the web_static
+folder of your AirBnB Clone repo, using the function do_pack.
+"""
 from fabric import task
-import time
+from datetime import datetime
+import os
 
 @task
 def do_pack(c):
-    """Generate a .tgz archive from the web_static folder."""
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+
+    Returns:
+        str: Archive path if generated successfully, None otherwise.
+    """
     try:
+        # Create the versions directory if it doesn't exist
         c.run("mkdir -p versions")
-        current_time = time.strftime("%Y%m%d%H%M%S")
-        c.run("tar -cvzf versions/web_static_{}.tgz web_static/".format(current_time))
-        return "versions/web_static_{}.tgz".format(current_time)
+
+        # Create the name of the .tgz file with current timestamp
+        date_format = "%Y%m%d%H%M%S"
+        current_time = datetime.now().strftime(date_format)
+        archive_name = "web_static_{}.tgz".format(current_time)
+
+        # Compress the web_static folder into the .tgz file
+        c.run("tar -cvzf versions/{} web_static".format(archive_name))
+
+        # Return the archive path if generated successfully
+        return "versions/{}".format(archive_name)
     except Exception as e:
         print("An error occurred:", e)
         return None
