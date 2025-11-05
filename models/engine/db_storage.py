6a53702d511bc40eb03463935529ddabbfabc26c
MICHAEL <michaelogada104@gmail.com>
2025-11-05 17:40:28 +0300

    task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,17 +9,17 @@ from models.base_model import Base
 from models.user import User
 from models.city import City
 
+
 class DBStorage:
     __engine = None
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-            .format(
-                os.environ.get('HBNB_MYSQL_USER'),
-                os.environ.get('HBNB_MYSQL_PWD'),
-                os.environ.get('HBNB_MYSQL_HOST'),
-                os.environ.get('HBNB_MYSQL_DB')),
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            os.environ.get('HBNB_MYSQL_USER'),
+            os.environ.get('HBNB_MYSQL_PWD'),
+            os.environ.get('HBNB_MYSQL_HOST'),
+            os.environ.get('HBNB_MYSQL_DB')),
             pool_pre_ping=True)
         if os.environ.get('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
@@ -55,8 +55,10 @@ class DBStorage:
 
     def reload(self):
         Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(sessionmaker(bind=self.__engine,
-            expire_on_commit=False))
+        self.__session = scoped_session(
+            sessionmaker(
+                bind=self.__engine, expire_on_commit=False))
+
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
